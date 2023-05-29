30056eae68c243433eb92cd8598f6378cc7cb141
biller24 <120118709+biller24@users.noreply.github.com>
2023-05-29 20:00:50 +0300

    AnnouncementPage

--- ApartEase/src/apartease/AnnouncementPage.java
+++ ApartEase/src/apartease/AnnouncementPage.java
@@ -2,10 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package announcementcreate;
-
-import apartease.AnnouncementCreatePage;
-
+package apartease;
+import java.sql.*;
 /**
  *
  * @author Bill
